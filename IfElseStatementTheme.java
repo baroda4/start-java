6101fcec323df6dfdb7db00fc5ec95cee1d02f8a
baroda4 <borodkinr@gmail.com>
2023-02-15 21:58:48 +0300

    Update IfElseStatementTheme.java

--- IfElseStatementTheme.java
+++ IfElseStatementTheme.java
@@ -1,6 +1,6 @@
 public class IfElseStatementTheme {
     public static void main(String[] args) {
-        System.out.println("1. Переврод псевдокласса на язык Java.");
+        System.out.println("1. Перевод псевдокласса на язык Java.");
         int age = 20;
         double height = 1.86;
         char gender = 'm';
